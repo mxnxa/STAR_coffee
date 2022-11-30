8cbecc71a558ad53b677402cfaff9a5b393664a8
mxnxa <alsgk0428@naver.com>
2022-11-30 16:28:38 +0900

    댓글 기능 추가

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import MainContent
+from .models import MainContent, Comment
 
 @admin.register(MainContent)
 class MainContentAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'pub_date']
     list_display_links = ['id', 'title']
+
+admin.site.register(Comment)

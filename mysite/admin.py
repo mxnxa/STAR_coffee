e4cc6261d6e6925bc315514758094a7ffce47c1f
mxnxa <alsgk0428@naver.com>
2022-12-07 20:22:14 +0900

    메인 페이지 슬라이드 '제품 소개 바로가기' 버튼 주소 매핑

--- mysite/admin.py
+++ mysite/admin.py
@@ -3,7 +3,8 @@ from .models import MainContent, Comment
 
 @admin.register(MainContent)
 class MainContentAdmin(admin.ModelAdmin):
-    list_display = ['title', 'content', 'pub_date']
+    list_display = ['id', 'title', 'content', 'pub_date']
+    list_display_links = ['id', 'title']
     search_fields = ['title']
 
 @admin.register(Comment)
